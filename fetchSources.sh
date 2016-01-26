3aa312d2700afe4c55fefb53b5837a1594395c61
rusak5 <rusak5@seznam.cz>
2016-01-26 20:27:12 +0100

    Update fetchSources.sh

--- fetchSources.sh
+++ fetchSources.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 [ "$(whoami)" != 'root' ] && ( echo you are using a non-privileged account; exit 1 )
 SUCESS=0
-#yum-config-manager --add-repo repository_url
-#yum-config-manager --enable repository
-if $SUCESS
-  then
-  touch fetchSucess
+#apache v2.4.12
+if [ dnf info httpd | sed -n '/Instal/,/Description/p' | grep Version | awk '{print $3}' != '2.4.12' ] ; then
+    
 fi
+
+exit $SUCESS
