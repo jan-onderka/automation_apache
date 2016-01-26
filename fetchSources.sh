bc06c4a90a2e493684e47e7774d2661e083595bc
root <root@localhost.localdomain>
2016-01-26 21:25:40 +0100

    a

--- fetchSources.sh
+++ fetchSources.sh
@@ -2,8 +2,10 @@
 [ "$(whoami)" != 'root' ] && ( echo you are using a non-privileged account; exit 1 )
 SUCESS=0
 #apache v2.4.12
-if [ dnf info httpd | sed -n '/Instal/,/Description/p' | grep Version | awk '{print $3}' != '2.4.12' ] ; then
+if [ "$(dnf info httpd | sed -n '/Instal/,/Description/p' | grep Version | awk '{print $3}')" != '2.4.12' ] ; then
     echo not work
+else 
+    echo works
 fi
 
 exit $SUCESS
