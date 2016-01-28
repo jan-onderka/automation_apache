d3e3c8ec5db1b2439686730ccc60240e8abe5f1d
rusak5 <rusak5@seznam.cz>
2016-01-28 20:55:12 +0100

    apr

--- fetchSources.sh
+++ fetchSources.sh
@@ -39,6 +39,7 @@ myAPP="apr"
 myVER="1.5.1"
 if [ "$(dnf info apr | sed -n '/Instal/,/Description/p' | grep Version | awk '{print $3}')" != '1.5.1' ] ; then
     dnf -y install apr-1.5.1
+    wget https://archive.apache.org/dist/apr/apr-1.5.1.tar.gz
 else 
     echo apr works
 fi
@@ -48,6 +49,7 @@ myAPP="apr-util"
 myVER="1.5.4"
 if [ "$(dnf info apr_util | sed -n '/Instal/,/Description/p' | grep Version | awk '{print $3}')" != '1.5.4' ] ; then
     dnf -y install apr_util-1.5.4
+    wget https://archive.apache.org/dist/apr/apr-util-1.5.1.tar.gz
 else 
     echo apr util works
 fi
